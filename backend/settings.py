dd3c87b3219510ea99305a5cd8a2b14c12b964bd
Franco Medina <franco02medina@gmail.com>
2026-04-23 15:03:12 +0800

    love u hanz

--- backend/settings.py
+++ backend/settings.py
@@ -166,8 +166,8 @@ AUTH_USER_MODEL = 'user.User'
 
 # ── Email ────────────────────────────────────────────────────────────────────
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_PORT          = 587
+EMAIL_HOST          = env('EMAIL_HOST', default='smtp-relay.brevo.com')
+EMAIL_PORT          = env.int('EMAIL_PORT', default=587)
 EMAIL_USE_TLS       = True
 EMAIL_HOST_USER     = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')

f990fb16394b8b4414d4a60bc773391a5909e75c
Franco Medina <franco02medina@gmail.com>
2026-05-02 14:42:23 +0800

    update

--- backend/settings.py
+++ backend/settings.py
@@ -172,7 +172,7 @@ EMAIL_HOST          = env('EMAIL_HOST', default='smtp-relay.brevo.com')
 EMAIL_PORT          = env.int('EMAIL_PORT', default=587)
 EMAIL_USE_TLS       = True
 EMAIL_USE_SSL       = False
-EMAIL_TIMEOUT       = 10
+EMAIL_TIMEOUT       = 30
 EMAIL_HOST_USER     = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL  = env('DEFAULT_FROM_EMAIL')
